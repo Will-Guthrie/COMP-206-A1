038af19cea3712820574b30c7436923770e0853c
Will Guthrie <william.guthrie@mail.mcgill.ca>
2018-02-04 12:30:32 -0500

    first commit

---
+++ COMP206_A1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]) {
+    if (argc != 4)
+    {
+        printf("Please submit exactly three arguments");
+        exit(-1);
+    }
+
+    FILE* shapeTemplate = fopen(argv[1], "r");
+    if (shapeTemplate = NULL) {
+
+    }
+
+}
